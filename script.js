63adac2bd7e7b13fd2d7067abea774469b583cf2
Hoàng Anh <anhace1101@gmail.com>
2022-06-27 23:09:58 +0700

    add keyboard listener

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ const switchPlayer = () => {
   player0El.classList.toggle("player--active");
   player1El.classList.toggle("player--active");
 };
+
 // 1.user roll dice
 btnRoll.addEventListener("click", function () {
   // generating the dice
@@ -97,4 +98,73 @@ btnNew.addEventListener("click", function () {
   document
     .querySelector(`.player--${activePlayer}`)
     .classList.remove("player--winner");
+});
+// add keyboard listener
+document.addEventListener("keypress", (e) => {
+  const keyPress = e.keyCode;
+  // console.log(keyPress);
+  // hold
+  if (keyPress === 13 || keyPress === 104) {
+    // 1. add current score to total score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. check score >= 100
+    if (scores[activePlayer] >= 100) {
+      // if yes => player win
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+      diceEl.classList.add("hidden");
+      btnHold.classList.add("hidden");
+      btnRoll.classList.add("hidden");
+    } else {
+      // if no => switch player
+      switchPlayer();
+    }
+  }
+  // roll dice
+  if (keyPress === 32 || keyPress === 114) {
+    // generating the dice
+    var dice = Math.floor(Math.random() * 6 + 1);
+    console.log(dice);
+
+    // 2. display the roll
+    diceEl.classList.remove("hidden");
+    diceEl.src = `./img/dice-${dice}.png`;
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+  // reset game
+  if (keyPress === 92 || keyPress == 110) {
+    //set all score = 0
+    scores = [0, 0];
+    currentScore = 0;
+    score0El.textContent = scores[0];
+    score1El.textContent = scores[1];
+    activePlayer = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    // remove win background
+    document.querySelector(`.player--0`).classList.remove("player--winner");
+    document.querySelector(`.player--1`).classList.remove("player--winner");
+    // remove active background
+    document.querySelector(`.player--1`).classList.remove("player--active");
+    //add active background for player 0
+    document.querySelector(`.player--0`).classList.add("player--active");
+    // display hold and roll btn
+    btnHold.classList.remove("hidden");
+    btnRoll.classList.remove("hidden");
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove("player--winner");
+  }
 });
